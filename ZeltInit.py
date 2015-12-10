b3e163e711974133fe9a39ed2592b7502772c659
Erik Seguinte <ErikSeguinte@me.com>
2015-12-09 20:56:46 -0800

    Format code

--- ZeltInit.py
+++ ZeltInit.py
@@ -1,38 +1,40 @@
 class Character:
-   """Class containing all character related variables."""
-   def __init__(self):
-       self.name=""
-       self.initiative=0
-       self.inert_initiative=False
-       self.crash_state=False
-       self.crash_counter=0
-       self.shift_target=""
-   def SetName(self):
-       self.name=input("Name: ")
+    """Class containing all character related variables."""
 
-   def set_init(self):
-       self.initiative=input("Join Battle Roll: ")
+    def __init__(self):
+        self.name = ""
+        self.initiative = 0
+        self.inert_initiative = False
+        self.crash_state = False
+        self.crash_counter = 0
+        self.shift_target = ""
+
+    def SetName(self):
+        self.name = input("Name: ")
+
+    def set_init(self):
+        self.initiative = input("Join Battle Roll: ")
 
 
 def clear_screen():
-   """Prints a bunch of new lines to clear the screen."""
-   for i in range(24):
-       print("")
+    """Prints a bunch of new lines to clear the screen."""
+    for i in range(24):
+        print("")
+
+
+def add_new_character():
+    """
+
+    :rtype: object
+    """
+    new_character = Character()
+    new_character.SetName()
+    new_character.set_init()
+    return new_character
+
 
 character_list = []
-def add_new_character():
-   """
-
-   :rtype: object
-   """
-   new_character=Character()
-   new_character.SetName()
-   new_character.set_init()
-   return new_character
-
-
 print("Hello World")
 clear_screen()
 character_list.append(add_new_character())
-print(character_list[0].name + " " +character_list[0].initiative)
-
+print(character_list[0].name + " " + character_list[0].initiative)
