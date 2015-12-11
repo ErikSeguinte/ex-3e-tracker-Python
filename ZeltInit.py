429caff79ab46ec57a2783e647639faa234938f7
Erik Seguinte <ErikSeguinte@me.com>
2015-12-11 01:45:14 -0800

    add NPC loop

--- ZeltInit.py
+++ ZeltInit.py
@@ -10,6 +10,21 @@ ITEMS = (
 )
 
 
+def dice_roller():
+    pool = input_validation.integer("Dice Pool: ")
+    successes = 0
+    i = 0
+    while i < pool:
+        d = randint(1, 10)
+        print(d)
+        if d >= 7:
+            successes += 1
+        if d == 10:
+            successes += 1
+        i += 1
+    return successes
+
+
 class Character:
     """Class containing all character related variables."""
 
@@ -21,12 +36,16 @@ class Character:
         self.crash_counter = 0
         self.shift_target = ""
         self.has_gone = False
+        self.join_battle = None
 
     def SetName(self):
         self.name = input("Name: ")
 
-    def set_init(self):
-        self.initiative = input_validation.integer("Join Battle: ")
+    def set_init(self, successes=None):
+        if successes == None:
+            self.initiative = input_validation.integer("Join Battle: ")
+        else:
+            self.initiative = successes
 
     def set_has_gone(self):
         self.has_gone = True
@@ -36,17 +55,6 @@ def clear_screen():
     """Prints a bunch of new lines to clear the screen."""
     for i in range(24):
         print("")
-
-
-def add_new_character():
-    """
-
-    :rtype: object
-    """
-    new_character = Character()
-    new_character.SetName()
-    new_character.set_init()
-    return new_character
 
 
 def add_players():
@@ -87,19 +95,23 @@ def sort_table(list):
     return sorted(sorted_list, key=lambda character: character.has_gone)
 
 
-def dice_roller():
-    pool = input_validation.integer("Dice Pool: ")
-    successes = 0
-    i = 0
-    while i < pool:
-        d = randint(1, 10)
-        print(d)
-        if d >= 7:
-            successes += 1
-        if d == 10:
-            successes += 1
-        i += 1
-    return successes
+def add_npc(name):
+    new_character = Character()
+    new_character.name = name
+    join_battle = input_validation.integer("Join Battle Dice Pool: ")
+    new_character.join_battle = join_battle
+    return new_character
+
+
+def add_new_character():
+    """
+
+    :rtype: object
+    """
+    new_character = Character()
+    new_character.SetName()
+    new_character.set_init()
+    return new_character
 
 
 print("Hello World")
@@ -113,5 +125,20 @@ while True:
     ui.print_menu()
     command = ui.get_command()
     if command is "Withering Attack":
-        print(command)
-    print(str(dice_roller()) + " successes!")
+        print("    " + command)
+    elif command is "Decisive Attack":
+        print("    " + command)
+    elif command is "Join Battle!":
+        print("    " + command)
+    elif command is "Add NPCs":
+        print("    " + "Adding NPCs")
+        max_loop = input_validation.integer("How many NPCs to add? ")
+        i = 0
+        print("Enter an empty line to quit.")
+        while i < max_loop:
+            name = input("New name: ")
+
+            # Empty strings are false.
+            if not name:
+                break
+            character_list.append(add_npc(name))
