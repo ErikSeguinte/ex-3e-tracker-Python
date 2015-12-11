d96da5753f8c96e66cadd6aea34c4acc277655b9
Erik Seguinte <ErikSeguinte@me.com>
2015-12-10 23:52:16 -0800

    Out of range raises ValueError

--- input_validation.py
+++ input_validation.py
@@ -1,14 +1,22 @@
 def integer(message, min_value=None, max_value=None):
     """Validates user input is valid integer.
+    :param max_value: max range, inclusive
+    :param min_value: minimum range, inclusive
+    :param message: input prompt
+    :return user_input
     """
     while True:
         try:
             user_input=int(input(message))
         except ValueError:
-           print("Invalid integer. Please try again.")
-           continue
+            print("Invalid integer. Please try again.")
+            continue
         else:
-            if (min_value is not None and user_input < min_value) or (max_value is not None and user_input > max_value):
+            try:
+                if (min_value is not None and user_input < min_value) or (
+                                max_value is not None and user_input > max_value):
+                    raise (ValueError)
+            except ValueError:
                 print("Out of range. Please try again.")
                 continue
             else:
--- ui.py
+++ ui.py
@@ -18,4 +18,4 @@ class UI:
     def get_command(self):
         """Gets input from user, converts to proper string."""
         command = input_validation.integer("Enter Command: ", 0, len(self.items))
-        print(str(command))
+        return self.items(command)
