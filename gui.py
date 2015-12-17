886c28b008f9291735817d9ec01a0bc6bd60504f
Erik Seguinte <ErikSeguinte@me.com>
2015-12-17 09:49:22 -0800

    Table View working

--- gui.py
+++ gui.py
@@ -1,6 +1,6 @@
 import sys
 from PyQt5 import QtGui, QtCore, QtWidgets
-import ZeltInit
+import ZeltInit as Z
 import main_window
 
 
@@ -13,6 +13,10 @@ class MainWindow(QtWidgets.QMainWindow, main_window.Ui_MainWindow):
         self.actionLoad_Players.triggered.connect(self.show_file_dialog)
         self.statusBar()
         # self.Withering_btn.
+        self.setup_model()
+
+
+        # self.model.setData(QtCore.QModelIndex(0,0),1)
 
     def print_stuff(self):
         print("OMG")
@@ -23,12 +27,35 @@ class MainWindow(QtWidgets.QMainWindow, main_window.Ui_MainWindow):
         print(fname[0])
 
         if fname[0]:
-            ZeltInit.add_players(fname[0])
+            Z.add_players(fname[0])
+
+        self.setup_model()
+
+    def setup_model(self):
+        Z.set_up_test()
+        character_list = Z.character_list
+        self.model = QtGui.QStandardItemModel(len(character_list), 5, self)
+        self.model.setHeaderData(0, QtCore.Qt.Horizontal, "Label")
+        self.table = self.tableView
+        self.tableView.setModel(self.model)
+        character_list = Z.character_list
+        print(character_list)
+
+        Z.sort_table()
+
+        self.model.setData(self.model.index(0, 0, QtCore.QModelIndex()), "1")
+
+        row = 0
+
+        for character in character_list:
+            self.model.setData(self.model.index(row, 0, QtCore.QModelIndex()), row)
+            self.model.setData(self.model.index(row, 1, QtCore.QModelIndex()), character.name)
+            self.model.setData(self.model.index(row, 2, QtCore.QModelIndex()), character.initiative)
+            self.model.setData(self.model.index(row, 3, QtCore.QModelIndex()), character.has_gone)
+            row += 1
 
 
-class InitView(QtWidgets.QAbstractItemView):
-    def __init__(self, parent=None):
-        super().__init__()
+
 
 
 
