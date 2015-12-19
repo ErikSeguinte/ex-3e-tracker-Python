a3d98411457b750160bd98ddb1ba517b25a0a4f8
Erik Seguinte <ErikSeguinte@me.com>
2015-12-19 15:04:42 -0800

    Shift target is populated. Still need to add Join Battle to it for players.

--- gui.py
+++ gui.py
@@ -140,8 +140,8 @@ class MainWindow(QtWidgets.QMainWindow, main_window.Ui_MainWindow):
             self.model.setData(self.model.index(row, 1, QtCore.QModelIndex()), character.initiative)
             self.model.setData(self.model.index(row, 2, QtCore.QModelIndex()), character.crash_state)
             self.model.setData(self.model.index(row, 3, QtCore.QModelIndex()), character.has_gone)
-            # if character.shift_target:
-            #     self.model.setData(self.model.index(row, 4, QtCore.QModelIndex()), character.shift_target.name)
+            if character.shift_target:
+                self.model.setData(self.model.index(row, 4, QtCore.QModelIndex()), character.shift_target.name)
             row += 1
 
         self.tableView.resizeColumnsToContents()
