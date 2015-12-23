a860bb176a261ee9115033507c66f0c141b66562
Erik Seguinte <ErikSeguinte@me.com>
2015-12-23 06:57:54 -0800

    First setup.py

--- setup.py
+++ setup.py
@@ -2,7 +2,11 @@ import sys
 from cx_Freeze import setup, Executable
 
 # Dependencies are automatically detected, but it might need fine tuning.
-build_exe_options = {'compressed': True,}
+build_exe_options = {
+    'compressed': True,
+    'includes':   [
+        'LICENSE.txt'
+    ],}
 
 # GUI applications require a different base on Windows (the default is for a
 # console application).
