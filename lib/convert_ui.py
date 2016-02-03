cb69d5c2e22f96813b48b020fb44570635d38da1
Erik Seguinte <ErikSeguinte@me.com>
2016-02-03 15:29:31 -0800

    started working on convert_ui to only run on modified files.

--- lib/convert_ui.py
+++ lib/convert_ui.py
@@ -1,7 +1,18 @@
 from PyQt5 import uic
-import sys
+import sys, re
 import os
 
 path = os.getcwd()
 
-uic.compileUiDir(path)
+# uic.compileUiDir(path)
+
+file_list = os.listdir(path)
+
+print(file_list)
+
+ui_files = [string for string in file_list if re.match(r'.*\.ui', string)]
+py_files = [string for string in file_list if
+            re.match(r'.*\.py', string) and not (string == '__init__.py' or string == 'convert_ui.py')]
+
+print(ui_files)
+print(py_files)
