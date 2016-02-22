05f63b1c11f8cf3dc1dd2ceaef4049d611a10f48
Erik Seguinte <ErikSeguinte@me.com>
2016-02-22 15:43:01 -0800

    updating Join Battle UI

--- Ex3-Tracker.py
+++ Ex3-Tracker.py
@@ -346,11 +346,64 @@ class MainWindow(QtWidgets.QMainWindow, main_window.Ui_MainWindow):
             print("New version available")
 
 
-class JoinBattleWindow(QtWidgets.QDialog, join_battle_gui.Ui_Dialog):
+# class JoinBattleWindow(QtWidgets.QDialog, join_battle_gui.Ui_Dialog):
+#     def __init__(self, name, parent=None, ):
+#         super().__init__()
+#         self.setupUi(self)
+#         self.groupBox.setTitle(name)
+#
+#     def exec(self):
+#         super().exec()
+#
+#         if self.result():
+#             return self.get_values()
+#         else:
+#             return None
+#
+#     def get_values(self):
+#         join_battle = self.spinBox.value()
+#         return join_battle
+
+
+class JoinBattleWindow(QtWidgets.QDialog):
     def __init__(self, name, parent=None, ):
-        super().__init__()
+        Dialog = super().__init__()
         self.setupUi(self)
         self.groupBox.setTitle(name)
+
+    def setupUi(self, Dialog):
+        Dialog.setObjectName("Dialog")
+        Dialog.resize(236, 70)
+        self.horizontalLayout = QtWidgets.QHBoxLayout(Dialog)
+        self.horizontalLayout.setObjectName("horizontalLayout")
+        self.groupBox = QtWidgets.QGroupBox(Dialog)
+        self.groupBox.setTitle("")
+        self.groupBox.setObjectName("groupBox")
+        self.formLayout = QtWidgets.QFormLayout(self.groupBox)
+        self.formLayout.setObjectName("formLayout")
+        self.label = QtWidgets.QLabel(self.groupBox)
+        self.label.setObjectName("label")
+        self.formLayout.setWidget(0, QtWidgets.QFormLayout.LabelRole, self.label)
+        self.spinBox = QtWidgets.QSpinBox(self.groupBox)
+        self.spinBox.setMinimum(-99)
+        self.spinBox.setObjectName("spinBox")
+        self.formLayout.setWidget(0, QtWidgets.QFormLayout.FieldRole, self.spinBox)
+        self.horizontalLayout.addWidget(self.groupBox)
+        self.buttonBox = QtWidgets.QDialogButtonBox(Dialog)
+        self.buttonBox.setOrientation(QtCore.Qt.Vertical)
+        self.buttonBox.setStandardButtons(QtWidgets.QDialogButtonBox.Cancel | QtWidgets.QDialogButtonBox.Ok)
+        self.buttonBox.setObjectName("buttonBox")
+        self.horizontalLayout.addWidget(self.buttonBox)
+
+        self.retranslateUi(Dialog)
+        self.buttonBox.accepted.connect(Dialog.accept)
+        self.buttonBox.rejected.connect(Dialog.reject)
+        QtCore.QMetaObject.connectSlotsByName(Dialog)
+
+    def retranslateUi(self, Dialog):
+        _translate = QtCore.QCoreApplication.translate
+        Dialog.setWindowTitle(_translate("Dialog", "Join Battle!"))
+        self.label.setText(_translate("Dialog", "Join Battle!"))
 
     def exec(self):
         super().exec()
